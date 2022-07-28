03cd279551c898555a4645af369a8c5304b687fa
Miroslav <miroslav.ievtushenko@gmail.com>
2022-07-28 19:46:10 +0200

    bubbleSorter

--- src/Main.java
+++ src/Main.java
@@ -47,3 +47,15 @@ public class Main {
         }
     }
 }
+
+/*
+private void bubbleSorter(){
+    for (int out = elems - 1; out >= 1; out--){
+        for(int in = 0; in < out; in++){
+            if(a[in] > a[in + 1]){
+            toSwap(in, in + 1);
+            }
+        }
+    }
+}
+ */
